c54979fb2bf55eba77d852847bc006ecbbce672e
cleitonaguiarandradecaa <cleitonaguiarandrade@gmail.com>
2025-02-25 13:30:19 -0300

    refactor: adjust position of directories

--- src/domain/data/protocols/usecases/add-account/db-add-account-protocols.ts
+++
@@ -1,4 +0,0 @@
-export * from "@/domain/data/protocols/criptography/hasher"
-export * from "@/domain/models/account"
-export * from "@/domain/usecases/add-account"
-export * from "@/domain/data/protocols/db/account/add-account-repositoy"
