d511e35ca2103c08b54d5a00a9c12b94dbb9b734
cleitonaguiarandradecaa <cleitonaguiarandrade@gmail.com>
2025-02-25 13:26:46 -0300

    feat: add data layer

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,15 @@
+import { Hasher, AddAccountRepository, AccountModel, AddAccountModel } from "./db-add-account-protocols"
+
+export class DbAddAccount implements DbAddAccount {
+  constructor (private readonly hasher: Hasher, private readonly addAccountRepository:
+  AddAccountRepository) {
+    this.hasher = hasher
+    this.addAccountRepository = addAccountRepository
+  }
+
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashPassword = await this.hasher.hash(accountData.password)
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashPassword }))
+    return new Promise(resolve => resolve(account))
+  }
+}
